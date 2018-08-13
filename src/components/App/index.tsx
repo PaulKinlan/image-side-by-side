6debdf82e1e2e8b11d60fd1341b04abac741b2e7
Paul Kinlan <paul.kinlan@gmail.com>
2018-08-13 09:00:32 +0100

    Fixing zooming issues + Also fixed formatting and linting issues

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -18,16 +18,16 @@ interface State {
 }
 
 enum ImageSide {
-  left,
-  right
+  left = 0,
+  right = 1,
 }
 
 export default class App extends Component<Props, State> {
   state: State = {
     leftLoaded: false,
-    rightLoaded: false
+    rightLoaded: false,
   };
-  
+
   canvasLeft?: HTMLCanvasElement;
   canvasRight?: HTMLCanvasElement;
 
@@ -43,10 +43,10 @@ export default class App extends Component<Props, State> {
       };
     }
   }
-  
+
   componentDidUpdate(prevProps: Props, prevState: State): void {
-    const {leftImage, rightImage} = this.state;
-    
+    const { leftImage, rightImage } = this.state;
+
     if (this.canvasLeft && leftImage) {
       drawBitmapToCanvas(this.canvasLeft, leftImage);
     }
@@ -58,63 +58,73 @@ export default class App extends Component<Props, State> {
   @bind
   async onFileChange(event: Event): Promise<void> {
     const fileInput = event.target as HTMLInputElement;
-    const side:ImageSide = (fileInput.classList.contains('left')) ? ImageSide.left : ImageSide.right;
+    const side:ImageSide = (fileInput.classList.contains('left')) ?
+      ImageSide.left : ImageSide.right;
     const file = fileInput.files && fileInput.files[0];
     if (!file) return;
-    
+
     await this.updateFile(file, side);
   }
 
   @bind
   async onFileDrop(event: FileDropEvent) {
     const { file } = event;
-    const side:ImageSide = ((event.target as HTMLElement).classList.contains('left')) ? ImageSide.left : ImageSide.right;
+    const side:ImageSide = ((event.target as HTMLElement).classList.contains('left')) ?
+      ImageSide.left : ImageSide.right;
     if (!file) return;
     await this.updateFile(file, side);
   }
 
   async updateFile(file: File, side:ImageSide) {
-    let newState = this.state; // this feels dirty
-    
-    if(side === ImageSide.left) {
+    const newState = this.state; // this feels dirty
+
+    if (side === ImageSide.left) {
       newState.leftImage = await createImageBitmap(file);
       newState.leftLoaded = true;
-    }
-    else {
+    } else {
       newState.rightImage = await createImageBitmap(file);
       newState.rightLoaded = true;
     }
-    
+
     this.setState(newState);
   }
 
   render({ }: Props, { leftLoaded, rightLoaded, error, leftImage, rightImage }: State) {
-    
-    const leftImageAttrs = leftImage ? { height: leftImage.height, width: leftImage.width } : {width: 300, height: 200};
-    const rightImageAttrs = rightImage ? { height: rightImage.height, width: rightImage.width } : {width: 300, height: 200};
+
+    const leftImageAttrs = leftImage ?
+      { height: leftImage.height, width: leftImage.width } : { width: 300, height: 200 };
+    const rightImageAttrs = rightImage ?
+      { height: rightImage.height, width: rightImage.width } : { width: 300, height: 200 };
     const leftImageStyles = { display: (leftLoaded) ? 'none' : 'block' };
-    const rightImageStyles = { display: (rightLoaded) ? 'none' : 'block'};
-     
+    const rightImageStyles = { display: (rightLoaded) ? 'none' : 'block' };
+
     return (
       <div class={style.app}>
-        <pinch-zoom class={style.left}>
-          <file-drop class='left' accept="image/*" onfiledrop={this.onFileDrop}>
-            <input class='left' type="file" onChange={this.onFileChange} style={ leftImageStyles } />
-            <canvas 
+        <file-drop class="left" accept="image/*" onfiledrop={this.onFileDrop}>
+          <input class="left" type="file" onChange={this.onFileChange} style={leftImageStyles} />
+          <pinch-zoom class={style.left}>
+            <canvas
               ref={linkRef(this, 'canvasLeft')}
               width={leftImageAttrs.width}
-              height={leftImageAttrs.height} />
-          </file-drop>
-        </pinch-zoom>
-        <pinch-zoom class={style.right}>
-          <file-drop class='right' accept="image/*" onfiledrop={this.onFileDrop}>
-            <input class='right' type="file" onChange={this.onFileChange} style={ rightImageStyles } />
-            <canvas 
+              height={leftImageAttrs.height}
+            />
+          </pinch-zoom>
+        </file-drop>
+        <file-drop class="right" accept="image/*" onfiledrop={this.onFileDrop}>
+          <input
+            class="right"
+            type="file"
+            onChange={this.onFileChange}
+            style={rightImageStyles}
+          />
+          <pinch-zoom class={style.right}>
+            <canvas
               ref={linkRef(this, 'canvasRight')}
               width={rightImageAttrs.width}
-              height={rightImageAttrs.height}/>
-          </file-drop>
-        </pinch-zoom>
+              height={rightImageAttrs.height}
+            />
+          </pinch-zoom>
+        </file-drop>
       </div>
     );
   }
